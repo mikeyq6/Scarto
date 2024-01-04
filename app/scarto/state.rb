d9a6ca035662dad0be6b638960aa8cedb5f21d2c
Michael Dann <michael.dann@singletrack.com>
2024-01-04 17:48:46 +0000

    add first_player to state and initialize current_trick

--- app/scarto/state.rb
+++ app/scarto/state.rb
@@ -1,8 +1,9 @@
 class State
-  attr_accessor :status, :current_player, :current_trick, :stock
+  attr_accessor :status, :current_player, :first_player, :current_trick, :stock
 
   def initialize
     @status = "Uninitialized"
     @stock = []
+    @current_trick = []
   end
 end
