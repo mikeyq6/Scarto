6e9a93b968e4a46f95e17ef6bf69ca6db67786fc
Michael Dann <michael.dann@singletrack.com>
2024-01-04 15:51:33 +0000

    add player types

--- app/scarto/player.rb
+++ app/scarto/player.rb
@@ -1,8 +1,18 @@
 class Player
   attr_accessor :name, :cards, :tricks
+  @@COMPUTER = 1
+  @@HUMAN = 2
+
   attr_reader :id
 
   def initialize
+  def self.COMPUTER
+    @@COMPUTER
+  end
+  def self.HUMAN
+    @@HUMAN
+  end
+
     @tricks = []
   end
 end
