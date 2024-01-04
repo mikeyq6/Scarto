078898d1931d7ac07196059c2d5dea365e39f2c4
Michael Dann <michael.dann@singletrack.com>
2024-01-04 15:52:37 +0000

    add stock to state and initializer

--- app/scarto/state.rb
+++ app/scarto/state.rb
@@ -1,4 +1,8 @@
 class State
-  attr_accessor :status, :current_player, :current_trick
+  attr_accessor :status, :current_player, :current_trick, :stock
 
+  def initialize
+    @status = "Uninitialized"
+    @stock = []
+  end
 end
