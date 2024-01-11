fee4a750d9764d2a9d091ddd74acdc23de9d6bc3
Michael Dann <michael.dann@singletrack.com>
2024-01-11 09:12:11 +0000

    add db schema

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'game#index'
+  
+  # get "/game", to: "game#index"
+  # get "/game/new", to: "game#new"
+  # get "/game/:id", to: "game#show"
+  # post "/game", to: "game#create"  # usually a submitted form
+  # get "/game/:id/edit", to: "game#edit"
+  # put "/game/:id", to: "game#update" # usually a submitted form
+  # delete "/game/:id", to: "game#destroy"
+
+  resources :game, only: [ :index, :show, :create ]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2024_01_10_102847) do
+
+  create_table "games", force: :cascade do |t|
+    t.string "firstname"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
