feda01aa47e3c62110d8be61d12e5012501fa68b
Michael Dann <michael.dann@singletrack.com>
2024-01-30 10:21:22 +0000

    improve ai

--- app/controllers/playgame_controller.rb
+++ app/controllers/playgame_controller.rb
@@ -132,12 +132,30 @@ class PlaygameController < ApplicationController
     player = @game.state.current_player
 
     # byebug
-    while !cardToPlay do
-      if @game.check_card_is_valid(player.hand, player.hand[index], @game.state.current_trick)
-        cardToPlay = player.hand[index]
-        @game.play_card(cardToPlay)
+    # Make it slightly smarter, play a low card if leading and a high card if playing
+    
+    if @game.state.current_trick.size == 0
+      while !cardToPlay do
+        if @game.check_card_is_valid(player.hand, player.hand[index], @game.state.current_trick)
+          cardToPlay = player.hand[index]
+          @game.play_card(cardToPlay)
+        end
+        index = index + 1
       end
-      index = index + 1
+    else
+      cardToPlay = Card.get_highest_card_of_suit(player.hand, @game.state.current_trick[0].suit)
+      
+      # if no card of suit, play lowest trump
+      if !cardToPlay
+        cardToPlay = Card.get_lowest_card_of_suit(player.hand, Card.suits[4])
+      end
+
+      # if not trump, play lowest card of remaining suits
+      if !cardToPlay
+        cardToPlay = Card.get_lowest_card_of_suit(player.hand, player.hand.first.suit)
+      end
+      
+      @game.play_card(cardToPlay)
     end
   end
 end
